894ae842b8642318cacd95fe11519f11ccefa16d
bonamin <bonamin.ike@gmail.com>
2023-01-14 12:55:57 -0300

    Fetch function Corrected

--- frontEnd/src/routes/+page.js
+++ frontEnd/src/routes/+page.js
@@ -1,18 +1,14 @@
-export async function load({ fetch }: any) {
-	try {
-		const res = await fetch('http://localhost:1337/api/posts?populate=deep');
-		const data = await res.json();
-		console.log('data', data);
-		console.log('body', { body: data });
-		const data2 = data.data;
-		console.log('data2', data2);
-
-		return { body: data };
-	} catch (err) {
-		console.log(`Error: ${err}`);
-	}
+export async function load({ fetch }) {
+    try {
+        const res = await fetch('http://localhost:1337/api/posts?populate=*');
+        const data = await res.json();
+        console.log(data);
+        return { body: data };
+    }
+    catch (err) {
+        console.log(`Error: ${err}`);
+    }
 }
-
 // export async function load() {
 // 	// The deep param is from the plugin https://market.strapi.io/plugins/strapi-plugin-populate-deep
 // 	// Normally you would use ?populate=* or other crazy option but could not get it working in short time
@@ -25,3 +21,4 @@ export async function load({ fetch }: any) {
 // 	const data = await response.json();
 // 	return data?.data;
 // }
+//# sourceMappingURL=+page.js.map
