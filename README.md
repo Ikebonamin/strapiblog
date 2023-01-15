6d11a00a86fa00884b99e32e20f06e4706695ae4
bonamin <bonamin.ike@gmail.com>
2023-01-15 10:24:14 -0300

    rendering corrected. moving on

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # strapiblog
 
 -> bad fetch request / corrected
--> Rendering issues. Data won´t render in {#foreach} blocks. Using svelte´s async load + export let data.
+-> Rendering issues. Data won´t render in {#foreach} blocks. Using svelte´s async load + export let data. / corrected
--- frontEnd/src/routes/+page.svelte
+++ frontEnd/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	export let data;
+	export let data: any;
 </script>
 
 <div class="container mx-auto mt-4">
@@ -9,12 +9,3 @@
 		<h1>No posts yet</h1>
 	{/each}
 </div>
-
-<!-- <div class="container mx-auto mt-4">
-	{#each data?.posts ?? [] as post}
-		<div class="hover:bg-gray-200 cursor-pointer px-6 py-2 border-b border-gray-500">
-			<h4 class="font-bold">{post.attributes.Titulo}</h4>
-			<h3 class="mt-2 text-gray-800">{post.attributes.Conteudo}</h3>
-		</div>
-	{/each}
-</div> -->
