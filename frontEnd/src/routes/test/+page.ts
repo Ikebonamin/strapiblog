a339c32b364e72a86d9541cadaba1ae14eec792b
bonamin <bonamin.ike@gmail.com>
2023-01-16 16:42:15 -0300

    Função fetch melhorada em /test

--- frontEnd/src/routes/test/+page.ts
+++ frontEnd/src/routes/test/+page.ts
@@ -1,9 +1,11 @@
 export async function load({ fetch }: any) {
 	try {
-		const response = await fetch('http://localhost:1337/api/posts');
-		const data = await response.json();
-		console.log('data from fetch: ', data);
-		return { posts: data?.data };
+		//Busca dados de posts do Strapi. Await espera resposta
+		let response = await fetch('http://localhost:1337/api/posts');
+		//Transforma resposta em json
+		response = await response.json();
+		//Retorna dados de posts. Criando um objeto com os dados de posts e metadados
+		return { posts: response.data, meta: response.meta };
 	} catch (err) {
 		console.log(`Error: ${err}`);
 	}
@@ -12,7 +14,7 @@ export async function load({ fetch }: any) {
 // }
 // console.log(data.data[0].attributes['Titulo']);
 ///////////////////////////////
-// bkp funcionando abaixo
+// bkp funcionando abaixo do Kevin
 // export async function load({ fetch }: any) {
 // 	try {
 // 		const response = await fetch('http://localhost:1337/api/posts');
